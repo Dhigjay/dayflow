3754d073e23766ac0f1c199ef7d67c6b781a72a7
Dhigjay <dhigjaya210@gmail.com>
2026-05-07 01:58:04 +0700

    launch icon

--- android/app/src/main/kotlin/com/new/packagename/MainActivity.kt
+++ android/app/src/main/kotlin/com/new/packagename/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.new.packagename
+package com.ti24a5.app8
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
